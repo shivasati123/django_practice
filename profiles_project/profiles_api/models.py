477b05a8f3f11303f1e3beb92b9dcfbff3d5f09a
AdarshSrivastava007 <adarsh.srivastava@sunking.com>
2024-04-23 17:03:29 +0530

    setup django admin

--- profiles_project/profiles_api/models.py
+++ profiles_project/profiles_api/models.py
@@ -26,7 +26,7 @@ class UserProfileManager(BaseUserManager):
 
           return user
       
-      def create_super_user(self,email,name,password):
+      def create_superuser(self,email,name,password):
           
           user = self.create_user(email, name, password)
 
